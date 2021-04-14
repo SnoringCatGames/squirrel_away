05d17d6f08c8619c80efbed949f5098c7ad8b227
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-04-14 13:53:33 -0700

    Add a loading image

---
+++ src/gui/LoadingImage.gd
@@ -0,0 +1,16 @@
+class_name LoadingImage
+extends Control
+
+var _base_scale := 1.0
+
+func set_base_scale(value: float) -> void:
+    _base_scale = value
+    update_gui_scale(_base_scale)
+
+func update_gui_scale(gui_scale: float) -> bool:
+    rect_position *= gui_scale
+    rect_min_size *= gui_scale
+    rect_size *= gui_scale
+    $Control.rect_position *= gui_scale
+    $Control/AnimatedSprite.scale *= gui_scale
+    return true
