807805d95cc831fb69bfc53774ed24d442192795
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-05-11 14:53:26 -0700

    Refactor where PlayerAnimator params get configured

--- src/players/cat/CatAnimator.gd
+++ src/players/cat/CatAnimator.gd
@@ -4,30 +4,6 @@ extends PlayerAnimator
 const HEAD_UNBLINK_REGION := Rect2(26, 39, 22, 18)
 const HEAD_BLINK_REGION := Rect2(47, 91, 22, 18)
 
-func _create_params() -> PlayerAnimatorParams:
-    var animator_params := PlayerAnimatorParams.new()
-    
-    animator_params.faces_right_by_default = false
-    
-    animator_params.rest_name = "Rest"
-    animator_params.rest_on_wall_name = "RestOnWall"
-    animator_params.jump_rise_name = "JumpRise"
-    animator_params.jump_fall_name = "JumpFall"
-    animator_params.walk_name = "Walk"
-    animator_params.climb_up_name = "Climb"
-    animator_params.climb_down_name = "Climb"
-
-    animator_params.rest_playback_rate = 0.8
-    animator_params.rest_on_wall_playback_rate = 0.8
-    animator_params.jump_rise_playback_rate = 1.0
-    animator_params.jump_fall_playback_rate = 1.0
-    animator_params.walk_playback_rate = 7.5
-    animator_params.climb_up_playback_rate = 9.0
-    animator_params.climb_down_playback_rate = \
-            -animator_params.climb_up_playback_rate / 2.33
-
-    return animator_params
-
 func move_torse_up() -> void:
     $Hip/Torso/TorsoSprite.z_index = 0
 
--- src/players/squirrel/SquirrelAnimator.gd
+++ src/players/squirrel/SquirrelAnimator.gd
@@ -1,29 +1,6 @@
 class_name SquirrelAnimator
 extends PlayerAnimator
 
-func _create_params() -> PlayerAnimatorParams:
-    var animator_params := PlayerAnimatorParams.new()
-    
-    animator_params.faces_right_by_default = true
-    
-    animator_params.rest_name = "Stand"
-    animator_params.rest_on_wall_name = "HoldWall"
-    animator_params.jump_rise_name = "JumpRise"
-    animator_params.jump_fall_name = "JumpFall"
-    animator_params.walk_name = "Run"
-    animator_params.climb_up_name = "ClimbUp"
-    animator_params.climb_down_name = "ClimbDown"
-    
-    animator_params.rest_playback_rate = 1.0
-    animator_params.rest_on_wall_playback_rate = 1.0
-    animator_params.jump_rise_playback_rate = 1.0
-    animator_params.jump_fall_playback_rate = 1.0
-    animator_params.walk_playback_rate = 2.3
-    animator_params.climb_up_playback_rate = 2.3
-    animator_params.climb_down_playback_rate = 2.3
-    
-    return animator_params
-
 func _play_animation(
         name: String,
         playback_rate: float = 1,
