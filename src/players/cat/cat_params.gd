838c0b8a82d9017b70ec9efe8a0ab43d2e293b27
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-07 11:38:32 -0700

    Clean up MovementParams flag names and doc comments.

--- src/players/cat/cat_params.gd
+++ src/players/cat/cat_params.gd
@@ -56,9 +56,9 @@ func _init_production_params() -> void:
     forces_player_velocity_to_zero_at_path_end = false
     syncs_player_position_to_edge_trajectory = true
     syncs_player_velocity_to_edge_trajectory = true
-    includes_discrete_frame_state = true
-    includes_continuous_frame_positions = true
-    includes_continuous_frame_velocities = true
+    includes_discrete_trajectory_state = true
+    includes_continuous_trajectory_positions = true
+    includes_continuous_trajectory_velocities = true
     bypasses_runtime_physics = false
     
     retries_navigation_when_interrupted = true
@@ -192,9 +192,9 @@ func _init_debug_params() -> void:
     syncs_player_position_to_edge_trajectory = true
     syncs_player_velocity_to_edge_trajectory = true
 #    syncs_player_velocity_to_edge_trajectory = false
-    includes_discrete_frame_state = true
-    includes_continuous_frame_positions = true
-    includes_continuous_frame_velocities = true
+    includes_discrete_trajectory_state = true
+    includes_continuous_trajectory_positions = true
+    includes_continuous_trajectory_velocities = true
     bypasses_runtime_physics = false
     retries_navigation_when_interrupted = true
 #    distance_squared_threshold_for_considering_additional_jump_land_points = 128.0 * 128.0
