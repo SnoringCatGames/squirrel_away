a554c1dccad0ef43ea3673ea76404652824bb849
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-08 12:17:45 -0700

    Remove trailing zeros after decimals in JSON encodings, and move JSON parsing logic into a separate file.

--- src/config/squirrel_away_config.gd
+++ src/config/squirrel_away_config.gd
@@ -285,8 +285,8 @@ var _player_param_classes := [
 var app_manifest := {
     # TODO: Remember to reset these when creating releases.
     precompute_platform_graph_for_levels = [
-        "6",
-        "7",
+#        "6",
+#        "7",
     ],
 #    ignores_platform_graph_save_files = true,
     ignores_platform_graph_save_files = false,
