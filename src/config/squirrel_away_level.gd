702e55786899c82457a8fa93f738a277627f609f
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-15 12:22:23 -0700

    Rename "human character" to "player character."

--- src/config/squirrel_away_level.gd
+++ src/config/squirrel_away_level.gd
@@ -27,7 +27,7 @@ func _ready() -> void:
 func _start() -> void:
     ._start()
     
-    cat = human_character
+    cat = player_character
 
     _does_level_have_squirrels = \
             Sc.level_config.get_level_config(Sc.level_session.id) \
