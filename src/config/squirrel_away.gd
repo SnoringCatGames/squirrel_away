7a2b6bb707a262f13f3610fd77988614cc2b0b9d
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-09-07 12:22:14 -0700

    Create another test level

--- src/config/squirrel_away.gd
+++ src/config/squirrel_away.gd
@@ -29,7 +29,7 @@ func _override_configs_for_current_run() -> void:
     _metadata.are_all_levels_unlocked = false
     _metadata.are_test_levels_included = true
     _metadata.is_save_state_cleared_for_debugging = false
-    _metadata.opens_directly_to_level_id = "8"
+    _metadata.opens_directly_to_level_id = "9"
     _metadata.is_splash_skipped = true
     _metadata.are_button_controls_enabled_by_default = false
     
@@ -40,6 +40,7 @@ func _override_configs_for_current_run() -> void:
 #        "6",
 #        "7",
 #        "8",
+#        "9",
     ]
     _surfacer_manifest.ignores_platform_graph_save_files = false
     
