1bc32f1d591238a179844d6c022ab2c8ff89f9e4
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-09-08 12:02:48 -0700

    Update character movement parameters.

--- src/config/squirrel_away.gd
+++ src/config/squirrel_away.gd
@@ -30,18 +30,18 @@ func _override_configs_for_current_run() -> void:
     _metadata.are_all_levels_unlocked = false
     _metadata.are_test_levels_included = true
     _metadata.is_save_state_cleared_for_debugging = false
-    _metadata.opens_directly_to_level_id = "8"
+    _metadata.opens_directly_to_level_id = "6"
     _metadata.is_splash_skipped = true
     _metadata.are_button_controls_enabled_by_default = false
     
     omit_squirrels = false
     
     _surfacer_manifest.precompute_platform_graph_for_levels = [
-#        "3",
-#        "6",
-#        "7",
-#        "8",
-#        "9",
+        "3",
+        "6",
+        "7",
+        "8",
+        "9",
     ]
     _surfacer_manifest.ignores_platform_graph_save_files = false
     
