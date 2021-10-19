49e88519311c4c4ed5d887e0358c0223db5d1f35
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-10-19 16:57:32 -0700

    Added some additional squirrels with different behaviors to level 8.

---
+++ src/levels/level8.gd
@@ -0,0 +1,22 @@
+tool
+class_name Level8
+extends SquirrelAwayLevel
+
+
+func _on_initial_input() -> void:
+    ._on_initial_input()
+    _attach_behavior_move_targets()
+
+
+func _attach_behavior_move_targets() -> void:
+    for squirrel in characters['squirrel']:
+        for behavior_class in [
+                    RunAwayBehavior,
+                    CollideBehavior,
+                    FollowBehavior,
+                ]:
+            var behavior: Behavior = \
+                    squirrel.get_behavior(behavior_class)
+            if is_instance_valid(behavior):
+                behavior.move_target = player_character
+                behavior.trigger(false)
