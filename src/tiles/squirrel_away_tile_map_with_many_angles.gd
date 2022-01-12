e924bf6b2435ea41d8f2c6b12300a20d487add84
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-01-12 10:16:38 -0800

    Continue prototyping a new tile-set system, with custom binding logic for internal subtiles.

--- src/tiles/squirrel_away_tile_map_with_many_angles.gd
+++ src/tiles/squirrel_away_tile_map_with_many_angles.gd
@@ -11,3 +11,17 @@ func _ready() -> void:
     if tile_set == null or \
             tile_set == Su.default_tile_set:
         tile_set = DEFAULT_TILE_SET
+    
+    connect("cell_tile_changed", self, "_on_cell_tile_changed")
+
+
+func _on_cell_tile_changed(
+        cell_position: Vector2,
+        next_tile_id: int,
+        previous_tile_id: int) -> void:
+    if next_tile_id == INVALID_CELL or \
+            previous_tile_id == INVALID_CELL:
+        call_deferred(
+                "update_bitmask_region",
+                cell_position + Vector2(-2, -2),
+                cell_position + Vector2(2, 2))
