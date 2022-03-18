43231fc10063981d0e5ef94ee9228a8140952d9a
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-18 14:56:26 -0700

    Consolidate framework metadata into schema classes.

---
+++ src/plugin/squirrel_away_manifest_schema.gd
@@ -0,0 +1,25 @@
+tool
+class_name SquirrelAwayManifestSchema
+extends FrameworkManifestSchema
+
+
+const _DISPLAY_NAME := "Squirrel Away"
+const _FOLDER_NAME := "squirrel_away"
+const _AUTO_LOAD_NAME := "SquirrelAway"
+const _AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
+const _AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
+const _ICON_DIRECTORY_PATH := "res://addons/squirrel_away/assets/images/"
+
+const _PROPERTIES := {
+}
+
+
+func _init().(
+        _DISPLAY_NAME,
+        _FOLDER_NAME,
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_DEPS,
+        _AUTO_LOAD_PATH,
+        _ICON_DIRECTORY_PATH,
+        _PROPERTIES) -> void:
+    pass
