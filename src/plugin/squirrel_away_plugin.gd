002488cd67f0b5cb6a8094909d368e41919389c3
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-18 11:56:22 -0700

    Create separate plugin boilerplate for each framework.

---
+++ src/plugin/squirrel_away_plugin.gd
@@ -0,0 +1,17 @@
+tool
+class_name SquirrelAwayPlugin
+extends FrameworkPlugin
+
+
+const _DISPLAY_NAME := "SquirrelAway"
+const _ICON_DIRECTORY_PATH := "res://addons/surfacer/assets/images/"
+const _AUTO_LOAD_NAME := "SquirrelAway"
+const _AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
+
+
+func _init().(
+        _DISPLAY_NAME,
+        _ICON_DIRECTORY_PATH,
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_PATH) -> void:
+    pass
