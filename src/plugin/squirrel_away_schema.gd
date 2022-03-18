932fed4fad14323c91b0600f44bc10866587359b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-18 15:51:42 -0700

    Add placeholder plugin editor-icons.

--- src/plugin/squirrel_away_schema.gd
+++ src/plugin/squirrel_away_schema.gd
@@ -8,7 +8,7 @@ const _FOLDER_NAME := "squirrel_away"
 const _AUTO_LOAD_NAME := "SquirrelAway"
 const _AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
 const _AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
-const _ICON_DIRECTORY_PATH := "res://addons/squirrel_away/assets/images/"
+const _PLUGIN_ICON_DIRECTORY_PATH := "res://addons/squirrel_away/assets/images/editor_icons/"
 
 const _PROPERTIES := {
 }
@@ -20,6 +20,6 @@ func _init().(
         _AUTO_LOAD_NAME,
         _AUTO_LOAD_DEPS,
         _AUTO_LOAD_PATH,
-        _ICON_DIRECTORY_PATH,
+        _PLUGIN_ICON_DIRECTORY_PATH,
         _PROPERTIES) -> void:
     pass
