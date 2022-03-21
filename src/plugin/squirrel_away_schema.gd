5a9a9224182921b228965db0d7d86907cbf26271
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-21 13:02:38 -0700

    Start adding support for toggling various manifest modes within the main-screen plugin.

--- src/plugin/squirrel_away_schema.gd
+++ src/plugin/squirrel_away_schema.gd
@@ -8,7 +8,9 @@ const _FOLDER_NAME := "squirrel_away"
 const _AUTO_LOAD_NAME := "SquirrelAway"
 const _AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
 const _AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
-const _PLUGIN_ICON_DIRECTORY_PATH := "res://addons/squirrel_away/assets/images/editor_icons/"
+const _MANIFEST_PATH := "res://addons/squirrel_away/src/config/manifest.json"
+const _PLUGIN_ICON_DIRECTORY_PATH := \
+        "res://addons/squirrel_away/assets/images/editor_icons/"
 
 const _PROPERTIES := {
 }
@@ -20,6 +22,7 @@ func _init().(
         _AUTO_LOAD_NAME,
         _AUTO_LOAD_DEPS,
         _AUTO_LOAD_PATH,
+        _MANIFEST_PATH,
         _PLUGIN_ICON_DIRECTORY_PATH,
         _PROPERTIES) -> void:
     pass
