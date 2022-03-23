832a35f9f6acf7fa696e1daeab69d79a89a14cf5
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-23 11:36:16 -0700

    Create a separate PluginMetadata class for accessing constant framework state.

---
+++ src/plugin/squirrel_away_metadata.gd
@@ -0,0 +1,26 @@
+tool
+class_name SquirrelAwayMetadata
+extends PluginMetadata
+
+
+const DISPLAY_NAME := "Squirrel_away"
+const FOLDER_NAME := "squirrel_away"
+const AUTO_LOAD_NAME := "SquirrelAway"
+const AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
+const AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
+const PLUGIN_ICON_DIRECTORY_PATH := \
+        "res://addons/squirrel_away/assets/images/editor_icons/plugin/"
+const SCHEMA_PATH := "res://addons/squirrel_away/src/plugin/squirrel_away_schema.gd"
+const MANIFEST_PATH := "res://addons/squirrel_away/src/config/manifest.json"
+
+
+func _init().(
+        DISPLAY_NAME,
+        FOLDER_NAME,
+        AUTO_LOAD_NAME,
+        AUTO_LOAD_DEPS,
+        AUTO_LOAD_PATH,
+        PLUGIN_ICON_DIRECTORY_PATH,
+        SCHEMA_PATH,
+        MANIFEST_PATH) -> void:
+    pass
--- src/plugin/squirrel_away_plugin.gd
+++ src/plugin/squirrel_away_plugin.gd
@@ -3,15 +3,8 @@ class_name SquirrelAwayPlugin
 extends FrameworkPlugin
 
 
-const _AUTO_LOAD_NAME := "SquirrelAway"
-const _AUTO_LOAD_PATH := \
-        "res://addons/squirrel_away/src/config/squirrel_away.gd"
-const _SCHEMA_PATH := \
-        "res://addons/squirrel_away/src/plugin/squirrel_away_schema.gd"
+const _METADATA_SCRIPT := SquirrelAwayMetadata
 
 
-func _init().(
-        _AUTO_LOAD_NAME,
-        _AUTO_LOAD_PATH,
-        _SCHEMA_PATH) -> void:
+func _init().(_METADATA_SCRIPT) -> void:
     pass
