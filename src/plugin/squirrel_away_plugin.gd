5b668b269046c9f456a86126e059c9495560a77c
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-23 10:02:59 -0700

    Move plugin files into a separate subdirectory; Create a new autoload for plugin-specific stuff; Decouple FrameworkPlugin from any transitive dependencies on FrameworkGlobal types.

--- src/plugin/squirrel_away_plugin.gd
+++ src/plugin/squirrel_away_plugin.gd
@@ -3,8 +3,15 @@ class_name SquirrelAwayPlugin
 extends FrameworkPlugin
 
 
-const _SCHEMA_CLASS := SquirrelAwaySchema
+const _AUTO_LOAD_NAME := "SquirrelAway"
+const _AUTO_LOAD_PATH := \
+        "res://addons/squirrel_away/src/config/squirrel_away.gd"
+const _SCHEMA_PATH := \
+        "res://addons/squirrel_away/src/plugin/squirrel_away_schema.gd"
 
 
-func _init().(_SCHEMA_CLASS) -> void:
+func _init().(
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_PATH,
+        _SCHEMA_PATH) -> void:
     pass
