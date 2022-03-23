e8e7ad4a34ac8e451d2e41a69a347e09dc154492
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-23 11:36:16 -0700

    Create a separate PluginMetadata class for accessing constant framework state.

--- src/plugin/squirrel_away_schema.gd
+++ src/plugin/squirrel_away_schema.gd
@@ -3,26 +3,11 @@ class_name SquirrelAwaySchema
 extends FrameworkSchema
 
 
-const _DISPLAY_NAME := "Squirrel Away"
-const _FOLDER_NAME := "squirrel_away"
-const _AUTO_LOAD_NAME := "SquirrelAway"
-const _AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
-const _AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
-const _MANIFEST_PATH := "res://addons/squirrel_away/src/config/manifest.json"
-const _PLUGIN_ICON_DIRECTORY_PATH := \
-        "res://addons/squirrel_away/assets/images/editor_icons/"
+const _METADATA_SCRIPT := SquirrelAwayMetadata
 
-const _PROPERTIES := {
+var _properties := {
 }
 
 
-func _init().(
-        _DISPLAY_NAME,
-        _FOLDER_NAME,
-        _AUTO_LOAD_NAME,
-        _AUTO_LOAD_DEPS,
-        _AUTO_LOAD_PATH,
-        _MANIFEST_PATH,
-        _PLUGIN_ICON_DIRECTORY_PATH,
-        _PROPERTIES) -> void:
+func _init().(_METADATA_SCRIPT, _properties) -> void:
     pass
