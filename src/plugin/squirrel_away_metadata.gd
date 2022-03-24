fab7bc01fe502631839fa479df4beb540a3e1161
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-24 09:52:05 -0700

    Update plugin metadata to support icons in different directories.

--- src/plugin/squirrel_away_metadata.gd
+++ src/plugin/squirrel_away_metadata.gd
@@ -3,13 +3,13 @@ class_name SquirrelAwayMetadata
 extends PluginMetadata
 
 
-const DISPLAY_NAME := "Squirrel_away"
+const DISPLAY_NAME := "Squirrel Away"
 const FOLDER_NAME := "squirrel_away"
 const AUTO_LOAD_NAME := "SquirrelAway"
 const AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
 const AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
-const PLUGIN_ICON_DIRECTORY_PATH := \
-        "res://addons/squirrel_away/assets/images/editor_icons/"
+const PLUGIN_ICON_PATH_PREFIX := \
+        "res://addons/scaffolder/assets/images/editor_icons/plugin/star"
 const SCHEMA_PATH := \
         "res://addons/squirrel_away/src/plugin/squirrel_away_schema.gd"
 const MANIFEST_PATH := "res://addons/squirrel_away/src/config/manifest.json"
@@ -21,7 +21,7 @@ func _init().(
         AUTO_LOAD_NAME,
         AUTO_LOAD_DEPS,
         AUTO_LOAD_PATH,
-        PLUGIN_ICON_DIRECTORY_PATH,
+        PLUGIN_ICON_PATH_PREFIX,
         SCHEMA_PATH,
         MANIFEST_PATH) -> void:
     pass
