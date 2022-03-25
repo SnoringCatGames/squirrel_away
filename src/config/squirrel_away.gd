fce765461970fc2c80b61c31d300856111386cf4
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-24 20:23:09 -0700

    Add a new threading mode; Update manifest overrides to use modes.

--- src/config/squirrel_away.gd
+++ src/config/squirrel_away.gd
@@ -6,10 +6,6 @@ extends SurfacerDefaultAppManifest
 const _SCHEMA_PATH := \
         "res://addons/squirrel_away/src/plugin/squirrel_away_schema.gd"
 
-var _is_using_pixel_style := true
-
-var _uses_threads := false and OS.can_use_threads()
-
 var _additional_metric_keys := [
     "start_new_squirrel_navigation",
 ]
@@ -28,23 +24,40 @@ func _get_members_to_destroy() -> Array:
 func _amend_manifest() -> void:
     ._amend_manifest()
     
-    # FIXME: LEFT OFF HERE: -----------------------------------------
+    # FIXME: LEFT OFF HERE: ------------------------------------------
+    # - Update values on `metadata` for the modes.
+    # - Update how/where the pixel-vs-anti-aliased style default manifests are
+    #   stored.
     
-    if _is_using_pixel_style:
-        Sc.gui_manifest.fonts_manifest = _default_fonts_manifest_pixel
-        Sc.styles_manifest = _styles_manifest_pixel
-        Sc.images_manifest = _default_images_manifest_pixel
-    else:
-        Sc.gui_manifest.fonts_manifest = _default_fonts_manifest_normal
-        Sc.styles_manifest = _default_styles_manifest_normal
-        Sc.images_manifest = _default_images_manifest_normal
+    var debug: bool = \
+            Sc.modes[FrameworkSchemaMode.RELEASE] == \
+            FrameworkSchemaMode.Release.LOCAL_DEV
+    var playtest: bool = \
+            Sc.modes[FrameworkSchemaMode.RELEASE] == \
+            FrameworkSchemaMode.Release.PLAYTEST
+    var is_using_threads: bool = \
+            Sc.modes[FrameworkSchemaMode.THREADING] == \
+            FrameworkSchemaMode.Threading.ENABLED
+    var is_using_pixel_style: bool = \
+            Sc.modes[FrameworkSchemaMode.UI_SMOOTHNESS] == \
+            FrameworkSchemaMode.UiSmoothness.PIXELATED
+    var are_annotations_emphasized: bool = \
+            Sc.modes[FrameworkSchemaMode.ANNOTATIONS] == \
+            FrameworkSchemaMode.Annotations.EMPHASIZED
+    
+    # FIXME: LEFT OFF HERE: ------------------------------------------
+#    if is_using_pixel_style:
+#        Sc.gui_manifest.fonts_manifest = _default_fonts_manifest_pixel
+#        Sc.styles_manifest = _styles_manifest_pixel
+#        Sc.images_manifest = _default_images_manifest_pixel
+#    else:
+#        Sc.gui_manifest.fonts_manifest = _default_fonts_manifest_normal
+#        Sc.styles_manifest = _default_styles_manifest_normal
+#        Sc.images_manifest = _default_images_manifest_normal
     
 #    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.default
     var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.full_screen
 #    var debug_window_size = ScaffolderGuiConfig.SCREEN_RESOLUTIONS.google_ads_portrait
-    
-    var are_annotations_emphasized := false
-#    var are_annotations_emphasized := true
     
     Sc.manifest.metadata.app_version = "0.0.1"
     
@@ -82,10 +95,12 @@ func _amend_manifest() -> void:
     Su.manifest.ignores_platform_graph_save_files = false
     
     Sc.metadata.thread_count = \
-            OS.get_processor_count() if Sc.metadata.uses_threads else 1,
+            OS.get_processor_count() if \
+            is_using_threads else \
+            1
     Su.manifest.uses_threads_for_platform_graph_calculation = \
             Su.manifest.uses_threads_for_platform_graph_calculation and \
-            Sc.metadata.uses_threads,
+            is_using_threads
 
     Sc.manifest.gui_manifest.debug_window_size = debug_window_size
     Sc.manifest.gui_manifest.hud_manifest.is_inspector_enabled_default = false
@@ -117,12 +132,13 @@ func _amend_manifest() -> void:
 #    }
     
     if are_annotations_emphasized:
-        _update_to_emphasize_annotations(app_manifest)
+        _set_common_overrides_for_annotations_mode()
     
-    _derive_overrides_according_to_debug_or_playtest(app_manifest)
+    _set_common_overrides_for_release_mode()
     
     Sc.manifest.character_manifest.omits_npcs = \
-            Sc.manifest.character_manifest.omits_npcs and _metadata.debug
+            Sc.manifest.character_manifest.omits_npcs and \
+            debug
 
 
 func _configure_sub_modules() -> void:
