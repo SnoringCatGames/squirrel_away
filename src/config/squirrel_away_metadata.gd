1fda401e37de3cc3c6727b568fe3fdd81acf90e5
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-30 11:16:23 -0700

    Move some files from config/ to global/; Move some configuration from globals to schema.

--- src/config/squirrel_away_metadata.gd
+++ src/config/squirrel_away_metadata.gd
@@ -7,12 +7,12 @@ const DISPLAY_NAME := "Squirrel Away"
 const FOLDER_NAME := "squirrel_away"
 const AUTO_LOAD_NAME := "SquirrelAway"
 const AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
-const AUTO_LOAD_PATH := "res://addons/squirrel_away/src/config/squirrel_away.gd"
+const AUTO_LOAD_PATH := "res://addons/squirrel_away/src/global/squirrel_away.gd"
 const PLUGIN_ICON_PATH_PREFIX := \
         "res://addons/scaffolder/assets/images/editor_icons/plugin/star"
 const SCHEMA_PATH := \
         "res://addons/squirrel_away/src/config/squirrel_away_schema.gd"
-const MANIFEST_PATH_OVERRIDE := "res://addons/squirrel_away/src/config/manifest.json"
+const MANIFEST_PATH_OVERRIDE := "res://addons/squirrel_away/src/global/manifest.json"
 const METRIC_KEYS := [
     "start_new_squirrel_navigation",
 ]
--- src/config/squirrel_away_schema.gd
+++ src/config/squirrel_away_schema.gd
@@ -12,6 +12,64 @@ var _additive_overrides := {
     ScaffolderSchema: {
         colors_manifest = \
             Utils.get_direct_color_properties(SquirrelAwayDefaultColors.new()),
+        annotation_parameters_manifest = Utils.get_direct_non_color_properties(
+            SquirrelAwayDefaultAnnotationParameters.new()),
+        music_manifest = [
+            # {
+            #     name = "my_music",
+            #     path_prefix = "res://addons/squirrel_away/assets/music/",
+            #     volume_db = 0.0,
+            #     bpm = 75.0,
+            #     meter = 4,
+            # },
+        ],
+        sounds_manifest = [
+            # {
+            #     name = "my_sound",
+            #     volume_db = 0.0,
+            #     path_prefix = "res://addons/squirrel_away/assets/sounds/",
+            # },
+        ],
+        gui_manifest = {
+            # FIXME: LEFT OFF HERE: ------------ Include this in all other frameworks too.
+            third_party_license_text = \
+                    SquirrelAwayThirdPartyLicenses.TEXT + \
+                    SurfaceTilerThirdPartyLicenses.TEXT + \
+                    ScaffolderThirdPartyLicenses.TEXT + \
+                    SurfacerThirdPartyLicenses.TEXT,
+            settings_item_manifest = {
+                groups = {
+                    annotations = {
+                        item_classes = [
+                        ],
+                    },
+                    hud = {
+                        item_classes = [
+                        ],
+                    },
+                    miscellaneous = {
+                        item_classes = [
+                        ],
+                    },
+                },
+            },
+            hud_manifest = {
+                hud_key_value_box_size = Vector2(256.0, 48.0),
+                hud_key_value_list_item_manifest = [
+                ],
+                is_hud_visible_by_default = true,
+                is_inspector_enabled_default = false,
+                inspector_panel_starts_open = false,
+            },
+            welcome_panel_manifest = {
+                items = [
+                ],
+            },
+            screen_manifest = {
+                screens = [
+                ],
+            },
+        },
     },
 }
 
