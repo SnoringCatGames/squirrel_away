dbfa6ebb0175ceb25243509b257167a322bffe35
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-30 09:37:58 -0700

    Add support for subtractive overrides in addition to the additive ones.

--- src/plugin/squirrel_away_schema.gd
+++ src/plugin/squirrel_away_schema.gd
@@ -8,13 +8,19 @@ const _METADATA_SCRIPT := SquirrelAwayMetadata
 var _properties := {
 }
 
-var _overrides := {
+var _additive_overrides := {
     ScaffolderSchema: {
         colors_manifest = \
             Utils.get_direct_color_properties(SquirrelAwayDefaultColors.new()),
     },
 }
 
+var _subtractive_overrides := {}
 
-func _init().(_METADATA_SCRIPT, _properties, _overrides) -> void:
+
+func _init().(
+        _METADATA_SCRIPT,
+        _properties,
+        _additive_overrides,
+        _subtractive_overrides) -> void:
     pass
