a1d17d4fbd4a7fb0156218c6429f44145edcacf2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-05 13:10:36 -0700

    Add a new home icon.

--- src/config/squirrel_away_metadata.gd
+++ src/config/squirrel_away_metadata.gd
@@ -9,7 +9,7 @@ const AUTO_LOAD_NAME := "SquirrelAway"
 const AUTO_LOAD_DEPS := ["Sc", "Su", "St"]
 const AUTO_LOAD_PATH := "res://addons/squirrel_away/src/global/squirrel_away.gd"
 const PLUGIN_ICON_PATH_PREFIX := \
-        "res://addons/scaffolder/assets/images/editor_icons/plugin/star"
+        "res://addons/scaffolder/assets/images/editor_icons/plugin/home"
 const SCHEMA_PATH := \
         "res://addons/squirrel_away/src/config/squirrel_away_schema.gd"
 const MANIFEST_PATH_OVERRIDE := "res://addons/squirrel_away/src/config/manifest.json"
